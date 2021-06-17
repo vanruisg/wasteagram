d91f51cd5f56855844b74f217cc87256b8ebf6e1
vanruisg <59535537+vanruisg@users.noreply.github.com>
2021-06-17 17:10:22 -0500

    Add files via upload

---
+++ models/posts.dart
@@ -0,0 +1,10 @@
+class Post {
+  DateTime date;
+  String imageURL;
+  double longitude;
+  double latitude;
+  int quantity;
+
+  Post(
+      {this.date, this.imageURL, this.longitude, this.latitude, this.quantity});
+}
